9fe64c5c6685b5a66fb9bd4a97572a07896585a9
Shammasahamedp <shammasahamedp123@gmail.com>
2024-07-23 18:15:59 +0530

    filter completed

--- controllers/user/userCategoryController.js
+++ controllers/user/userCategoryController.js
@@ -6,43 +6,50 @@ const { default: mongoose } = require('mongoose');
 const getCategory = async (req, res) => {
     try {
         const page = parseInt(req.query.page) || 1;
-        const limit = parseInt(req.query.limit) || 9; 
+        const limit = parseInt(req.query.limit) || 9;
         const skip = (page - 1) * limit;
         // Number.MAX_SAFE_INTEGER
-        console.log(req.query.minPrice)
-
+        // console.log(req.query.minPrice)
+        const sortBy=req.query.sort||'name'
+        // console.log(sortBy)
         const minPrice = parseInt(req.query.minPrice) || 0;
-        const maxPrice = parseInt(req.query.maxPrice) || 1000000;
+        const maxPrice = parseInt(req.query.maxPrice) || 50000;
 
         // const categorySlug = req.query.category || ''; 
-        const categoryId = req.query.categoryId ? req.query.categoryId : ''
-        const searchQuery=req.query.searchQuery
+        const categoryId = req.query.categoryId
+        console.log(categoryId)
+        const searchQuery = req.query.searchQuery
         const filter = {
             price: { $gte: minPrice, $lte: maxPrice },
-            isListed:true,
+            isListed: true,
             $or: [
                 { name: { $regex: new RegExp(searchQuery, 'i') } },
                 { 'category.name': { $regex: new RegExp(searchQuery, 'i') } }
-            ]
+            ],
+           
+
         };
-        
-        
-            if (categoryId&&  mongoose.Types.ObjectId.isValid(categoryId)) {
-                filter['category.id'] =new mongoose.Types.ObjectId(categoryId);
-            }
-        
-            console.log(filter)
-        const productsQuery =  Product.find(filter).skip(skip).limit(limit)
-        const countQuery =  Product.countDocuments(filter);
-        const categoryquery = Category.find({listed:true})
+        let sorT
+        if(sortBy==='Price Low To High'){
+            sorT={price:1}
+        }else if(sortBy==='Price High To Low'){
+            sorT={price:-1}
+        }
+        if (categoryId && mongoose.Types.ObjectId.isValid(categoryId)) {
+            filter['category.id'] = new mongoose.Types.ObjectId(categoryId);
+        }
+        console.log(filter)
+        const productsQuery = Product.find(filter).sort(sorT).skip(skip).limit(limit)
+        const countQuery = Product.countDocuments(filter);
+        const categoryquery = Category.find({ listed: true })
         const [products, totalProducts, categories] = await Promise.all([
             productsQuery.exec(),
             countQuery.exec(),
             categoryquery.exec()
         ]);
-
-        if(req.session.user){
-            console.log(maxPrice)
+        // console.log(count)
+        if (req.session.user) {
+            // console.log(maxPrice)
             return res.status(200).render('users/category', {
                 products,
                 currentPage: page,
@@ -50,11 +57,14 @@ const getCategory = async (req, res) => {
                 categories,
                 minPrice,
                 maxPrice,
-                categorySlug:categoryId||'',
+                searchQuery,
+                totalProducts,
+                sortBy,
+                categorySlug: categoryId || '',
                 user: req.session.user
             });
         } else {
-            console.log(maxPrice)
+            // console.log(maxPrice)
             return res.status(200).render('users/category', {
                 products,
                 currentPage: page,
@@ -62,7 +72,10 @@ const getCategory = async (req, res) => {
                 categories,
                 minPrice,
                 maxPrice,
-                categorySlug:categoryId||'',
+                sortBy,
+                searchQuery,
+                totalProducts,
+                categorySlug: categoryId || '',
                 user: ''
             });
         }
@@ -71,17 +84,7 @@ const getCategory = async (req, res) => {
         res.status(500).json({ error: 'Internal server error' });
     }
 };
-// const searchProduct=async (req,res)=>{
-//     try{
-//         const query=req.body.searchQuery
-//         console.log(query)
-//         const products=await Product.find({$or:[{name:{$regex:new RegExp(query,'i')}},{'category.name':{$regex:new RegExp(query,'i')}}]})
-//         console.log(products)
-//         res.status(200).json({message:'successfully reached here',products})
-//     }catch(err){
-//         console.error(err)
-//     }
-// }
+
 
 module.exports = {
     getCategory,
--- controllers/user/userOrderController.js
+++ controllers/user/userOrderController.js
@@ -1,52 +1,75 @@
 const { errorMonitor } = require('nodemailer/lib/xoauth2')
-const Order=require('../../models/ordersModel')
-const Address=require('../../models/addressModel')
-const mongoose=require('mongoose')
-const getOrder=async(req,res)=>{
-    try{
-        const userId=req.session.user
-        const orders=await Order.find({userId}).populate('items.productId')
-        console.log('sdfgsdfg',orders)
-        res.status(200).render('orders/usersOrder',{orders})
-    }catch(err){
-        console.error(err)
-    }
+const Order = require('../../models/ordersModel')
+const Address = require('../../models/addressModel')
+const mongoose = require('mongoose')
+const getOrder = async (req, res) => {
+  try {
+    const userId = req.session.user
+    const orders = await Order.find({ userId }).populate('items.productId')
+    // console.log('sdfgsdfg',orders)
+    res.status(200).render('orders/usersOrder', { orders })
+  } catch (err) {
+    console.error(err)
+  }
 }
-const getOrderDetails=async(req,res)=>{
-    try{
-        const userId=req.session.user
-        const orders1=await Order.findOne({userId})
-        console.log('asdfasd',orders1)
-        const orders=await Order.findOne({userId}).populate('items.productId')
-        console.log('asdfasdfasdf',orders)
-        const addressId=orders.addressId
-        console.log(addressId)
-        const address=await Address.aggregate([
-            { $match: { 
-                 userId:new mongoose.Types.ObjectId(userId),
-                'address._id':{
-                  $in:[new mongoose.Types.ObjectId(orders.addressId)]
-                }
-             } },
-            { 
-              $project: {
-                address: {
-                  $filter: {
-                    input: "$address",
-                    as: "add",
-                    cond: { $eq: ["$$add._id", [new mongoose.Types.ObjectId(orders.addressId)]] }
-                  }
-                }
-              }
+const getOrderDetails = async (req, res) => {
+  try {
+    const userId = req.session.user
+    const orders1 = await Order.findOne({ userId })
+    console.log('asdfasd', orders1)
+    const orders = await Order.findOne({ userId }).populate('items.productId')
+    console.log('asdfasdfasdf', orders)
+    const addressId1 = orders.addressId
+    console.log('addressId', addressId1)
+    console.log('userId', userId)
+    // const address=await Address.aggregate([
+    //     { $match: { 
+    //          userId:new mongoose.Types.ObjectId(userId),
+    //         'address._id':{
+    //           $in:[new mongoose.Types.ObjectId(addressId1)]
+    //         }
+    //      } },
+    //     { 
+    //       $project: {
+    //         address: {
+    //           $filter: {
+    //             input: "$address",
+    //             as: "add",
+    //             cond: { $eq: ["$$add._id", [new mongoose.Types.ObjectId(addressId1)]] }
+    //           }
+    //         }
+    //       }
+    //     }
+    //   ])
+    const address = await Address.aggregate([
+      {
+        $match: {
+          userId: new mongoose.Types.ObjectId(userId),
+          'address._id': new mongoose.Types.ObjectId(addressId1)
+        }
+      },
+      {
+        $project: {
+          address: {
+            $filter: {
+              input: "$address",
+              as: "add",
+              cond: { $eq: ["$$add._id", new mongoose.Types.ObjectId(addressId1)] }
             }
-          ])
-        console.log(address)
-        res.status(200).render('orders/orderDetails',{orders})
-    }catch(err){
-        console.error(err)
-    }
+          }
+        }
+      }
+    ]);
+
+    console.log(address[0].address);
+
+    console.log('this is address:', address)
+    res.status(200).render('orders/orderDetails', { orders ,address})
+  } catch (err) {
+    console.error(err)
+  }
 }
-module.exports={
-    getOrder,
-    getOrderDetails
+module.exports = {
+  getOrder,
+  getOrderDetails
 }
