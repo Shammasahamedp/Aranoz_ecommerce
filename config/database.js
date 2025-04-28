c919383fab3acf620da7382bff77d37d3edc4bc7
Shammas Ahamed p <159105756+Shammasahamedp@users.noreply.github.com>
2025-04-28 09:42:03 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose')
 require('dotenv').config()
-const dbURI=process.env.MONGODB_URI ||'mongodb+srv://shammas:0SHOT5BuqhNAFEmW@aranoz.gyg65.mongodb.net/aranozecommerce?retryWrites=true&w=majority&appName=Aranoz'
+const dbURI=process.env.MONGODB_URI 
 const connectDB=async()=>{
     try{
         await mongoose.connect(dbURI,{
